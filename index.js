516b66198f6a6d03ae70eeb943101fca811136f6
Collins Lubwama <bizzicole87@gmail.com>
2022-09-21 12:19:54 +0300

    cracklePop

---
+++ index.js
@@ -0,0 +1,30 @@
+//A program that prints out the numbers 1 to 100 (inclusive). 
+// function CracklePop(){
+// for(i = 0; i < 100; i++);{
+//     //If the number is divisible by 3, print Crackle instead of the number.
+//     if(i % 3 === 0){
+//         console.log("Crackle")
+//     }
+//     //If it's divisible by 5, print Pop. 
+//     else if(i % 5 === 0){
+//         console.log("Pop")
+//     }
+//     //If it's divisible by both 3 and 5, print CracklePop.
+//     else if(i % 3 === 0 && i % 5 === 0){
+//         console.log("cracklePop")
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+// }
+
+// CracklePop()
+
+let i = 3;
+
+if(i < 1){
+    console.log("shit")
+}
+ 
+ 
